ae59625888ce773fa7e973b297b6f9ceefc8ec1e
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-05-23 23:13:41 -0300

    create deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+async function main() {
+  const [deployer] = await hre.ethers.getSigners()
+  const accountBalance = await deployer.getBalance()
+
+  console.log("deploy contract with account:", deployer.address)
+  console.log("account balance:", accountBalance.toString())
+
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
+
+  const waveContract = await waveContractFactory.deploy()
+  await waveContract.deployed()
+
+  console.log('contract deployed to:', waveContract.address)
+}
+
+async function runDeployScript() {
+  try {
+    await main()
+
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+
+    process.exit(1)
+  }
+}
+
+runDeployScript()
