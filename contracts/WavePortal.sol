aade6c6f422146e2c4dc77a7ce75377b1ba5cacf
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-05-23 21:20:25 -0300

    add WavePortal contract functions/variables and update script to run the contract functions

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("my first solidity smart contract");
+    }
+
+    function addWave() public {
+        totalWaves += 1;
+
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("we have %d waves now!", totalWaves);
+
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,27 @@
 async function main() {
+  const [owner, randomPerson] = await hre.ethers.getSigners()
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
 
   const waveContract = await waveContractFactory.deploy()
   await waveContract.deployed()
 
   console.log('contract deployed to:', waveContract.address)
-}  
+  console.log('contract deployed by:', owner.address)
+
+  let wavesCount
+
+  wavesCount = await waveContract.getTotalWaves()
+
+  let addWave = await waveContract.addWave()
+  await addWave.wait()
+
+  wavesCount = await waveContract.getTotalWaves()
+
+  addWave = await waveContract.connect(randomPerson).addWave()
+  await addWave.wait()
+
+  wavesCount = await waveContract.getTotalWaves()
+}
 
 async function runMainScript() {
   try {
