bd2fbb41fb757b1678eba881d36be93ca7d30680
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-05-24 10:55:52 -0300

    setup chackra-ui to web project

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { theme } from "./styles/theme";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider resetCSS theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
