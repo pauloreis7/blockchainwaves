49540edb34b541781742b5c45ea1be6d770e9b48
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-05-24 11:10:37 -0300

    create initial page layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,21 @@
-export default function App() {
-  return <h1>Hello World!</h1>;
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+export function App() {
+  return (
+    <Flex w="100%" h="100vh" flexDirection="column" alignItems="center" p="14">
+      <Heading
+        textAlign="center"
+        mb="4"
+        textTransform="uppercase"
+        fontSize="5xl"
+        color="blue.500"
+      >
+        the waves portal
+      </Heading>
+
+      <Text textAlign="center" fontSize="2xl" color="gray.400" fontWeight="500">
+        The amazing portal where you can wave to your friends by blockchain!
+      </Text>
+    </Flex>
+  );
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "./styles/theme";
 
-import App from "./App";
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
