0d2dc281fc32d44c21850b1768680bb5e6bc7419
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-05-27 12:57:45 -0300

    update smart contract to store waves

--- blockchain/contracts/WavePortal.sol
+++ blockchain/contracts/WavePortal.sol
@@ -7,17 +7,34 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    event NewWaveEvent(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waverAddress;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
     constructor() {
         console.log("my first solidity smart contract");
     }
 
-    function addWave() public {
+    function addWave(string memory _message) public {
         totalWaves += 1;
-
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWaveEvent(msg.sender, block.timestamp, _message);
     }
 
-    function getTotalWaves() public view returns (uint256) {
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWavesCount() public view returns (uint256) {
         console.log("we have %d waves now!", totalWaves);
 
         return totalWaves;
--- blockchain/scripts/run.js
+++ blockchain/scripts/run.js
@@ -1,26 +1,24 @@
 async function main() {
-  const [owner, randomPerson] = await hre.ethers.getSigners()
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
-
   const waveContract = await waveContractFactory.deploy()
   await waveContract.deployed()
 
-  console.log('contract deployed to:', waveContract.address)
-  console.log('contract deployed by:', owner.address)
+  console.log('deployed contract address:', waveContract.address)
 
   let wavesCount
+  wavesCount = await waveContract.getTotalWavesCount()
+  console.log('wavesCount', wavesCount.toNumber())
 
-  wavesCount = await waveContract.getTotalWaves()
+  let addWaveTransaction = await waveContract.addWave("Ex message!")
+  await addWaveTransaction.wait()
 
-  let addWave = await waveContract.addWave()
-  await addWave.wait()
+  const [_, randomPerson] = await hre.ethers.getSigners()
 
-  wavesCount = await waveContract.getTotalWaves()
+  addWaveTransaction = await waveContract.connect(randomPerson).addWave("Ex message2!")
+  await addWaveTransaction.wait()
 
-  addWave = await waveContract.connect(randomPerson).addWave()
-  await addWave.wait()
-
-  wavesCount = await waveContract.getTotalWaves()
+  let allwaves = await waveContract.getAllWaves()
+  console.log('allwaves', allwaves)
 }
 
 async function runMainScript() {
